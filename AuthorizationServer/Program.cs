d5f6a016232330c0ff945564aac0d00d16188f14
Abhilash <abhilashgraja@gmail.com>
2022-09-23 20:11:29 +0530

    Code the credentials control flow

--- AuthorizationServer/Program.cs
+++ AuthorizationServer/Program.cs
@@ -1,4 +1,6 @@
+using AuthorizationServer;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
@@ -8,6 +10,55 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 {
     options.LoginPath = "/account/login";
 });
+
+builder.Services.AddDbContext<DbContext>(options =>
+{
+    // Configure the context to use an in-memory store.
+    options.UseInMemoryDatabase(nameof(DbContext));
+
+    // Register the entity sets needed by OpenIddict.
+    options.UseOpenIddict();
+});
+
+builder.Services.AddOpenIddict()
+
+        // Register the OpenIddict core components.
+        .AddCore(options =>
+        {
+            // Configure OpenIddict to use the EF Core stores/models.
+            options.UseEntityFrameworkCore().UseDbContext<DbContext>();
+        })
+
+        // Register the OpenIddict server components.
+        .AddServer(options =>
+        {
+            options.AllowClientCredentialsFlow();
+
+            options
+                .AllowAuthorizationCodeFlow()
+                .RequireProofKeyForCodeExchange();
+
+            options
+                  .SetAuthorizationEndpointUris("/connect/authorize")
+                  .SetTokenEndpointUris("/connect/token");
+
+            // Encryption and signing of tokens
+            options
+                .AddEphemeralEncryptionKey()
+                .AddEphemeralSigningKey()
+                .DisableAccessTokenEncryption();
+
+            // Register scopes (permissions)
+            options.RegisterScopes("api");
+
+            // Register the ASP.NET Core host and configure the ASP.NET Core-specific options.
+            options
+                .UseAspNetCore()
+                .EnableTokenEndpointPassthrough()
+                .EnableAuthorizationEndpointPassthrough();
+        });
+
+builder.Services.AddHostedService<TestHostedService>();
 
 var app = builder.Build();
 
